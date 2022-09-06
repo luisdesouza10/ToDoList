91fabfad8118ea06d5c1e00bf5eaf02c3cb53b0c
Luis Eduardo <luis@leaderweb.com.br>
2022-09-06 09:58:29 -0300

    feat: Input and button to add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import styles from "./App.module.css";
 import { Header } from "./components";
+import plus from "./assets/plus.svg";
+import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <form className={styles.addTaskContainer}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button>
+          <strong>Criar</strong>
+          <img src={plus} />
+        </button>
+      </form>
 
       <div className={styles.wrapper}></div>
     </div>
