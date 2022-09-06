4cb1dc5b3163a408c2acc20a9687b72921aafef1
Luis Eduardo <luis@leaderweb.com.br>
2022-09-06 08:59:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+import { Header } from "./components";
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      {/* <Header /> */}
+
+      <div className={styles.wrapper}></div>
+    </div>
+  );
+}
+
+export default App;
